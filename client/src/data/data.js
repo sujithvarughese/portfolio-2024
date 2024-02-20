eee48717954f66e4bb75cdddaea7e63fb09e55f4
Sujith Varughese <sujithv15@gmail.com>
2024-02-20 13:54:33 -0500

    about me section grammatical fixes

--- client/src/data/data.js
+++ client/src/data/data.js
@@ -5,11 +5,12 @@ export const projectsText = [
   'Note: Cross-site tracking will need to be disabled if using Safari'
 ]
 
-export const aboutMe = 'While I have always been obsessed with anything tech-related, I started my official journey into software in 2020 when I decided to put my goals in motion by pursing a degree in computer science. While in school, I quickly became fascinated by the fact that I could build complex structures from scratch to function together to perform virtually any task. (Seems a bit obvious when I put it in writing, but bear with me.)'
+export const aboutMe = 'While I have always been obsessed with anything tech-related, I started my official journey into software in 2020 when I decided to put my goals in motion by pursuing a degree in computer science. While in school, I quickly became fascinated by the fact that I could build complex structures from scratch to function together to perform virtually any task. (Seems a bit obvious when I put it in writing, but bear with me.)'
 
-export const aboutMe2 = 'To transition these abstractions over into the real world, I started building web applications in Javascript. My recent projects (React, NodeJS, MongoDB) were created with a focus on building with scalability. The creative process has guided me to gain a solid understanding of how a library such as React works under the hood which I believe has drastically increased by ability to create efficiently. The deployment experience gave me the opportunity to familiarize myself with the complete software life cycle process including testing and maintenance, while giving me exposure to network and DNS server configurations, which I thoroughly enjoyed.'
+export const aboutMe2 = 'To transition these abstractions over into the real world, I started building web applications in Javascript. My recent projects (React, NodeJS, MongoDB) were created with a focus on building with scalability. The creative process has guided me to gain a solid understanding of how a library such as React works under the hood which I believe has drastically increased my ability to create efficiently. The deployment experience gave me the opportunity to familiarize' +
+  ' myself with the complete software life cycle process including testing and maintenance, while giving me exposure to network and DNS server configurations, which I thoroughly enjoyed.'
 
 
 const s = ".'"
-export const aboutMe3 = "Aside from programming, in my free time I enjoy working out, creating and editing videos, brushing up on my DJ skills, and hanging out with (mostly spoiling) my 10 year old rottweiler, Hera!"
+export const aboutMe3 = "Aside from programming, in my free time I enjoy working out, playing basketball, creating and editing videos, and hanging out with (mostly spoiling) my 10 year old rottweiler, Hera!"
 export const skills = ['Javascript', 'Python', 'Java', 'React', 'NodeJS', 'RESTful APIs', 'API integration', 'Express', 'MongoDB', 'SQL', 'Data Structures', 'Software Testing', 'App deployment', 'DNS config', 'HTML/CSS', 'Bootstrap', 'Tailwind', 'Git version control']
