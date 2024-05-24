50421d077cc0777bdb085aedda0ca2ad0448b6d3
Sujith Varughese <sujithv15@gmail.com>
2024-05-24 18:58:19 -0400

    updates to layouts and styling

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap'
 const Contact = () => {
   return (
     <section>
-      <Container>
+      <Container style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "12px" }}>
         <h2>Contact Me</h2>
         <h4>Feel free to contact me with any feedback or work opportunities. Thanks!</h4>
       </Container>
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,22 +1,26 @@
-import { Container, Col, Row } from 'react-bootstrap'
+import { Container, Col, Row, Stack } from 'react-bootstrap'
 import linkedInIcon from '../assets/icons/linkedin-icon.svg';
 import fbIcon from '../assets/icons/fb-icon.svg';
 import githubIcon from "../assets/icons/github-mark-white.svg"
 const Footer = () => {
   return (
     <footer className="footer">
-      <Container>
-        <Row className="align-items-center">
-          <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
-              <a href="http://linkedin.com/sujithvarughese" className="social-icon" target="_blank" rel="noreferrer"><img src={linkedInIcon} alt="Icon" /></a>
-              <a href="http://facebook.com/sujithvarughese" className="social-icon" target="_blank" rel="noreferrer"><img src={fbIcon} alt="Icon" /></a>
-              <a href="http://github.com/sujithvarughese" className="social-icon" target="_blank" rel="noreferrer"><img src={githubIcon} alt="Icon" /></a>
-            </div>
-            <p style={style}>Copyright 2024. All Rights Reserved</p>
-          </Col>
-        </Row>
-      </Container>
+
+      <Stack className="text-center">
+
+        <div className="social-icon">
+          <a href="http://linkedin.com/sujithvarughese" target="_blank" rel="noreferrer"><img src={linkedInIcon} alt="Icon" /></a>
+          <a href="http://facebook.com/sujithvarughese" target="_blank" rel="noreferrer"><img src={fbIcon} alt="Icon" /></a>
+          <a href="http://github.com/sujithvarughese" target="_blank" rel="noreferrer"><img src={githubIcon} alt="Icon" /></a>
+        </div>
+
+        <div>
+          <p style={style}>Copyright 2024. All Rights Reserved</p>
+        </div>
+
+
+      </Stack>
+
     </footer>
   )
 }
