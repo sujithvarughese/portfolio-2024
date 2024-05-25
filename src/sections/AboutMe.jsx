d756b36ad659db154afcc3b1ff5f0ec2a428d283
Sujith Varughese <sujithv15@gmail.com>
2024-05-25 02:25:35 -0400

    bootstrap grid fixes

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -6,24 +6,27 @@ import profilePicture from "../assets/images/profile/profile_studio.png"
 const AboutMe = () => {
   return (
     <section className={classes.aboutMe} id="about">
-      <Container>
+      <Container className="container-fluid">
         <h2>About Me</h2>
-        <Container className="d-flex flex-column flex-lg-row justify-content-around">
+        <div className="row">
+          <div className="col-12 col-lg-6">
+
+            <div className={`${classes.image} col`}>
+              <Image src={profilePicture} alt="Profile Picture" />
+            </div>
+
+            <div className={`${classes.paragraph} col`}>
+              <p>{aboutMe}</p>
+              <p>{aboutMe2}</p>
+              <p>{aboutMe3}</p>
+            </div>
+          </div>
 
           {/*<div className={`${classes.text} ${classes.highlight}`}>
             The creative process has guided me to gain a solid understanding of how a library such as React works <strong>under the hood</strong> which I believe has drastically increased my ability to <strong>create efficiently</strong>.
           </div>*/}
-          <Container className={`${classes.image}`}>
-            <Image src={profilePicture} alt="Profile Picture" />
-          </Container>
 
-          <Container className={`${classes.paragraph}`}>
-            <p>{aboutMe}</p>
-            <p>{aboutMe2}</p>
-            <p>{aboutMe3}</p>
-          </Container>
-
-        </Container>
+        </div>
       </Container>
     </section>
   )
