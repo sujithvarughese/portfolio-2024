ae921ba5cda2418139ae2932c95825cbedfd92bc
Sujith Varughese <sujithv15@gmail.com>
2024-05-26 07:48:01 -0400

    mobile navi bar

--- src/components/NavBarFloat.jsx
+++ src/components/NavBarFloat.jsx
@@ -1,9 +1,19 @@
 import classes from '../styles/NavBarFloat.module.css'
 import { motion, useMotionValueEvent, useScroll } from 'framer-motion'
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Container, Nav, Navbar, NavDropdown, Stack } from 'react-bootstrap'
 import { useEffect, useState } from 'react'
 import { IoIosHome, IoIosApps } from "react-icons/io";
 import { RiUser3Fill } from "react-icons/ri";
+import { PiCertificateFill } from "react-icons/pi";
+import { FaFacebookF, FaGithub } from "react-icons/fa6";
+
+import diploma from "../assets/images/certificates/diploma.png"
+import metaCert from "../assets/images/certificates/meta_frontend_cert.png"
+import javascriptCert from "../assets/images/certificates/adv_javascript_cert.jpg"
+import reactNativeCert from "../assets/images/certificates/react_native_cert.jpg"
+import mernCert from "../assets/images/certificates/MERN_2024_cert.jpg"
+import accountingCert from "../assets/images/certificates/accounting_certificate.png"
+import businessManagementCert from "../assets/images/certificates/business_management_cert.png"
 
 
 const NavBarFloat = () => {
@@ -26,9 +36,9 @@ const NavBarFloat = () => {
   const onUpdateActiveLink = (value) => setActiveLink(value)
 
   return (
-      <Nav
+      <Navbar
         as={motion.nav}
-        className={classes.container}
+        className={classes.navbar}
         variants={{
           visible: { y: "20%" },
           hidden: { y: "-100%" }
@@ -36,31 +46,52 @@ const NavBarFloat = () => {
         animate={isHidden ? "hidden" : "visible"}
         transition={{ duration: 0.5, ease: "easeInOut" }}
       >
-        <Container className={classes.links}>
-          <Nav.Link
-            href="#home"
-            className={activeLink === "home" ? classes.active : ""}
-            onClick={() => onUpdateActiveLink("home")}
+        <Stack direction="horizontal"
+          className={classes.container}
+        >
+          <NavDropdown
+            title={<PiCertificateFill size="28px" color="#fff"/>}
           >
-            <IoIosHome size="24px" />
-          </Nav.Link>
-          <Nav.Link
-            href="#about"
-            className={activeLink === "about" ? classes.active : ""}
-            onClick={() => onUpdateActiveLink("about")}
-          >
-            <RiUser3Fill size="24px" />
-          </Nav.Link>
-          <Nav.Link
-            href="#projects"
-            className={activeLink === "projects" ? classes.active : ""}
-            onClick={() => onUpdateActiveLink("projects")}
-          >
-            <IoIosApps size="24px" />
-          </Nav.Link>
-        </Container>
+            <NavDropdown.Item href={diploma} target="_blank" rel="noreferrer">Bachelor's Diploma</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href={metaCert} target="_blank" rel="noreferrer">Meta Front End Developer</NavDropdown.Item>
+            <NavDropdown.Item href={javascriptCert} target="_blank" rel="noreferrer">Advanced Javascript</NavDropdown.Item>
+            <NavDropdown.Item href={reactNativeCert} target="_blank" rel="noreferrer">React Native 2024</NavDropdown.Item>
+            <NavDropdown.Item href={mernCert} target="_blank" rel="noreferrer">MERN Stack 2024</NavDropdown.Item>
+            <NavDropdown.Item href={accountingCert} target="_blank" rel="noreferrer">Accounting Technology</NavDropdown.Item>
+            <NavDropdown.Item href={businessManagementCert} target="_blank" rel="noreferrer">Business Management</NavDropdown.Item>
+          </NavDropdown>
+          <Nav className={classes.links}>
+            <Nav.Link
+              href="#home"
+              className={activeLink === "home" ? classes.active : ""}
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              <IoIosHome size="24px" />
+            </Nav.Link>
+            <Nav.Link
+              href="#about"
+              className={activeLink === "about" ? classes.active : ""}
+              onClick={() => onUpdateActiveLink("about")}
+            >
+              <RiUser3Fill size="24px" />
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={activeLink === "projects" ? classes.active : ""}
+              onClick={() => onUpdateActiveLink("projects")}
+            >
+              <IoIosApps size="24px" />
+            </Nav.Link>
+          </Nav>
+          <Stack direction="horizontal" className={classes.socials}>
+            <a href="http://facebook.com/sujithvarughese" target="_blank" rel="noreferrer"><FaFacebookF size="24px"/></a>
+            <a href="http://github.com/sujithvarughese" target="_blank" rel="noreferrer"><FaGithub size="24px"/></a>
+          </Stack>
+        </Stack>
 
-      </Nav>
+
+      </Navbar>
 
 
 
